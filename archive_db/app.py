bcbba70dd613b012de710ca09148d9d3da86687c
Johan Hermansson <johan.hermansson@medsci.uu.se>
2017-12-01 16:49:41 +0100

    Remove unecessary slash in end of URL

--- archive_db/app.py
+++ archive_db/app.py
@@ -18,7 +18,7 @@ def routes(**kwargs):
 
     return [
         url(r"/api/1.0/version", VersionHandler, name="version", kwargs=kwargs),
-        url(r"/api/1.0/upload/", UploadHandler, name="upload"),
+        url(r"/api/1.0/upload", UploadHandler, name="upload"),
         url(r"/api/1.0/verifification/([\w_-]+)",
             VerificationHandler, name="verification", kwargs=kwargs),
         url(r"/api/1.0/removal/([\w_-]+)", RemovalHandler, name="removal", kwargs=kwargs)
